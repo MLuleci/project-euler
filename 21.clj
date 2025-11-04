54c123c9dcaaa2a35c67f4a6d8642389c0f34bcb
Mert Luleci <mert.luleci@bp.com>
2025-11-04 14:35:44 +0000

    Problem 21

---
+++ 21.clj
@@ -0,0 +1,13 @@
+(require '[clojure.math :as math])
+
+(defn factors [n]
+  (cons 1 
+        (->> (range 2 (math/ceil (math/sqrt n)))
+            (filter #(= (mod n %) 0))
+            (mapcat #(list % (/ n %)))
+            (distinct))))
+
+(defn d [n] (reduce + (factors n)))
+(def m (zipmap (range 10000) (map d (range 10000))))
+
+(println (time (reduce-kv (fn [acc k v] (if (and (not= (m k) k) (= (m v) k)) (+ acc k) acc)) 0 m)))
