6db1d915a93ef993e96a112f807b0e1dcd05c6f2
Mert Luleci <mert.luleci@bp.com>
2025-11-11 12:47:57 +0000

    Problem 23

---
+++ 23.clj
@@ -0,0 +1,19 @@
+(require '[clojure.math :as math])
+
+(def sum-of-factors
+  (memoize (fn [n] 
+    (if (= n 1) 1
+        (let [l (int (math/ceil (math/sqrt n)))]
+          (->> (range 2 l)
+               (filter #(= (mod n %) 0))
+               (map #(+ % (/ n %)))
+               (reduce + (inc (if (= (* l l) n) l 0)))))))))
+
+(defn abundant? [n]
+  (> (sum-of-factors n) n))
+
+(def LIMIT 28124)
+(def ABUNDANTS (filter abundant? (range 1 LIMIT)))
+(def PAIRS (into #{} (mapcat (fn [x] (filter #(< % LIMIT) (map #(+ x %) ABUNDANTS))) ABUNDANTS)))
+
+(println (time (reduce + (filter #(nil? (PAIRS %)) (range 1 LIMIT)))))
