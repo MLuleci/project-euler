3a05ea5c000180d2dd9faf8d22002fb766cce34a
MLuleci <mert@luleci.net>
2026-05-13 12:58:42 +0100

    Problem 31

---
+++ src/project_euler/solutions/31.clj
@@ -0,0 +1,25 @@
+; Classic dynamic programming problem, with two solutions:
+; 1. Top-down recursive algorithm with memoization
+; 2. Bottom-up matrix-based dp algorithm
+; The former is easier to implement in a functional language like Clojure,
+; so I'm going to do the latter :)
+
+(ns project-euler.solutions.31)
+
+(def coins [1 2 5 10 20 50 100 200]) ; in pence
+
+(defn make-change
+  ([n] (make-change n coins (repeat (inc n) 0)))
+  ([n [coin & rest] memo]
+    (if (not coin)
+        (last memo) ; 0 -> n
+        (make-change n rest 
+          (vec (reduce (fn [acc i]
+                        (conj acc (if (> coin i)
+                                      (nth memo i) ; can't use the coin
+                                      (+ (nth memo i)             ; don't use the coin
+                                         (nth acc (- i coin)))))) ; use the coin
+                       [1]
+                       (range 1 (inc n))))))))
+
+(defn -main [] (make-change 200))
