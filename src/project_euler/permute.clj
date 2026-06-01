8c88af430a804af396611b5fbae2dae7e90f1abc
MLuleci <mert@luleci.net>
2026-06-01 15:29:02 +0100

    Lazy permutation generator
    Used lexicographical order to lazily generate permutations.

--- src/project_euler/permute.clj
+++ src/project_euler/permute.clj
@@ -1,17 +1,28 @@
 (ns project-euler.permute)
 
-(defn swap [i j coll]
-  (let [ei (nth coll i)
-        ej (nth coll j)]
-    (assoc (assoc coll i ej) j ei)))
+(defn max-index [f & xs]
+  (last (keep-indexed #(when %2 %1) (apply map f xs))))
+
+(defn swap [coll i j]
+  (-> coll
+      (assoc i (nth coll j))
+      (assoc j (nth coll i))))
+
+(defn render [coll indices]
+  (map #(nth coll %) indices))
+
+(defn next-order [p]
+  (when-let [k (max-index < p (rest p))]
+    (let [pk (nth p k)
+          l (max-index #(< pk %) p)
+          [a b] (split-at (inc k) (swap p k l))]
+      (vec (concat a (reverse b))))))
 
 (defn permute 
-  ([coll] (permute (count coll) (vec coll)))
-  ([k coll] (cond (<= k 0) nil
-                  (= k 1) (list coll)
-                  :else (reduce (fn [acc index]
-                                  (let [head (first acc)]
-                                    (concat (permute (- k 1) (swap (if (even? k) index 0) (- k 1) head)) acc)))
-                                (permute (- k 1) coll)
-                                (range 0 (- k 1))))))
-
+  ([coll] (permute coll (vec (range (count coll)))))
+  ([coll p]
+    (lazy-seq
+      (cons (render coll p)
+            (when-let [n (next-order p)]
+              (permute coll n))))))
+        
